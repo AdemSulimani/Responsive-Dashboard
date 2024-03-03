5dd9ff58cb806296eac1e3e0d91416d40eedf65f
Adem Sulimani <162042413+AdemSulimani@users.noreply.github.com>
2024-03-03 17:38:49 +0100

    Add files via upload

---
+++ Dashboard/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+
+})
+
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", () => {
+    preloader.classList.add("remove")
+})
